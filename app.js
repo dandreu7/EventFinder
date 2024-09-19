a291d9addb0e477a0044123904dc335e22d84bcf
Kieran St-Onge <kieran.stonge@icloud.com>
2024-09-18 22:58:35 -0400

    Working main page skeleton
    Using ejs, this now works as a skeleton

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Define routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
