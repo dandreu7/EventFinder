42c9f3c9860825faaeba8883e19ba05376f7eacf
Kieran <kstonge@uncc.edu>
2024-11-13 12:05:17 -0500

    fixed error message in login

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const eventRoutes = require('./routes/eventRoutes');
+const session = require('express-session'); //npm i express-session
 const userRoutes = require('./routes/userRoutes');
 const MongoStore = require('connect-mongo');
+const isAuthenticated = require('./middleware/auth'); // Adjust path as needed
 // For form data parsing
 // const bodyParser = require('body-parser');
 const app = express();
@@ -15,6 +17,18 @@ let url = `mongodb+srv://${process.env.USER}:${process.env.PASS}@eventcluster.7i
 
 // Middleware to parse form data
 app.use(express.urlencoded({ extended: true }));
+// Protect routes that require login
+app.use('/protected-route', isAuthenticated, eventRoutes); // Example of using middleware on event routes
+
+app.use(
+    session({
+        secret: '75377306233c1c3e8fac85a4ed4820a311c7ddf9066a4658785d30a3833f8cfc72752894f2d09e0bfb8be0191a249907b5842cce21e5ad924681a88e68b3b628',
+        resave: false,
+        saveUninitialized: false,
+        cookie: {maxAge: 60*60*1000}, //60 mins, 60 secs, 1000 milliseconds = 1 hour
+        store: new MongoStore({mongoUrl: url}) //same url as connecting to database
+    })
+);
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
