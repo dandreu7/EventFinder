50c2eae2956c302e4e28d75cc0f3b8fa6396b90b
dandreu7 <dandreu01@gmail.com>
2024-11-26 17:57:58 -0500

    Update fileUpload.js

--- middleware/fileUpload.js
+++ middleware/fileUpload.js
@@ -1,4 +1,5 @@
 const multer = require("multer"); // Multer for file uploads
+const path = require('path');
 
 // Set up Multer for file uploads
 const storage = multer.diskStorage({
@@ -12,6 +13,22 @@ const storage = multer.diskStorage({
 //const upload = multer({ storage: storage });
 
 // File Filter may need to be added in future
-exports.upload = multer({storage,
-                        limits:{fileSize: 4*1024*1024}
+exports.upload = multer({
+                        storage,
+                        limits:{fileSize: 4*1024*1024},
+                        fileFilter: function(req, file, cb) {
+                          checkFileType(file, cb);
+                        }
 });
+// Check file type
+function checkFileType(file, cb) {
+  const filetypes = /jpeg|jpg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Images only! (jpeg, jpg, png)');
+  }
+}
