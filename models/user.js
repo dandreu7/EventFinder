47ed461400859e2d3bd91bb1f896ff34beb95aec
Kieran <kstonge@uncc.edu>
2024-11-26 17:19:46 -0500

    fixed header

--- models/user.js
+++ models/user.js
@@ -1,32 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
-    firstName: {type: String, required: [true, 'cannot be empty']},
-    lastName: {type: String, required: [true, 'cannot be empty']},
-    email: { type: String, required: true, unique: true, lowercase: true },
-    password: {type: String, required: [true, 'cannot be empty']},
-    rsvpedEvents: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Event' }]
+  firstName: { type: String, required: [true, "cannot be empty"] },
+  lastName: { type: String, required: [true, "cannot be empty"] },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: [true, "cannot be empty"] },
+  bio: { type: String, default: "" },
+  rsvpedEvents: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
 });
 
-//encrypt passwords using hash before saving to database
-userSchema.pre('save', function (next) {
-    let user = this;
-    if (!user.isModified('password')) {
-        return next();
-    }
-    bcrypt.hash(user.password, 10)
-        .then(hash => {
-            user.password = hash;
-            next();
-        })
-        .catch(err => next(err));
+// Encrypt passwords using hash before saving to database
+userSchema.pre("save", function (next) {
+  let user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt
+    .hash(user.password, 10)
+    .then((hash) => {
+      user.password = hash;
+      next();
+    })
+    .catch((err) => next(err));
 });
 
-//method to compare login password to stored hash
+// Method to compare login password to stored hash
 userSchema.methods.comparePassword = function (loginPassword) {
-    return bcrypt.compare(loginPassword, this.password);
+  return bcrypt.compare(loginPassword, this.password);
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
